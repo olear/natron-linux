4e773747ae8715928b0340a02c1bb37a7b3308af
Ole Andre Rodlie <olear@dracolinux.org>
2014-08-12 03:41:18 +0200

    add plugins in package.

--- scripts/build-package-freebsd.sh
+++ scripts/build-package-freebsd.sh
@@ -3,7 +3,7 @@
 # Written by Ole Andre Rodlie <olear@dracolinux.org>
 #
 
-VERSION=Preview1
+VERSION=Preview2
 
 CWD=$(pwd)
 TMP=$CWD/tmp
@@ -19,6 +19,7 @@ cp -av $INSTALL_PATH/share/OpenColor* $TMP/$TGZ/share/ || exit 1
 cp -av $INSTALL_PATH/docs/* $TMP/$TGZ/docs/ || exit 1
 cp -av $INSTALL_PATH/lib/libcairo.so.11202 $TMP/$TGZ/lib/ || exit 1
 rm -rf $TMP/$TGZ/docs/cairo/*GPL*
+cp -av $INSTALL_PATH/Plugins $TMP/$TGZ/ || exit 1
 
 cat $CWD/installer/Natron-BSD.sh > $TMP/$TGZ/Natron || exit 1
 cat $CWD/installer/Natron-BSD.sh | sed "s#bin/Natron#bin/NatronRenderer#" > $TMP/$TGZ/NatronRenderer || exit 1
