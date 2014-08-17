d04a27ab1ff2ccaad01fee2b4823c877e8c376e9
Ole Andre Rodlie <olear@dracolinux.org>
2014-08-17 02:52:23 +0200

    merge linux and freebsd

--- scripts/build-plugins-release.sh
+++ scripts/build-plugins-release.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 #
+# Build Natron Core Plug-ins for Linux and FreeBSD.
 # Written by Ole Andre Rodlie <olear@dracolinux.org>
 #
 
@@ -8,9 +9,8 @@ sleep 5
 
 GIT_IO=https://github.com/MrKepzie/openfx-io.git
 GIT_MISC=https://github.com/devernay/openfx-misc.git
-
-IO_V=b9dd66d0a1c3babe00a6944a1ed052f08fe86dae
-MISC_V=30bd8fd6249cfd4b24807d9da5f4863ef0820f6d
+IO_V=$(cat IO_RELEASE)
+MISC_V=$(cat MISC_RELEASE)
 SDK_VERSION=1.0
 
 # Threads
@@ -22,6 +22,7 @@ fi
 if [ -z "$ARCH" ]; then
   case "$( uname -m )" in
     i?86) export ARCH=i686 ;;
+    amd64) export ARCH=x86_64 ;;
        *) export ARCH=$( uname -m ) ;;
   esac
 fi
@@ -34,17 +35,25 @@ elif [ "$ARCH" = "x86_64" ]; then
 else
   BF="-O2"
 fi
+OS=$(uname -o)
 CWD=$(pwd)
-INSTALL_PATH=/opt/Natron-$SDK_VERSION
+
+if [ "$OS" == "GNU/Linux" ]; then
+  INSTALL_PATH=/opt/Natron-$SDK_VERSION
+else
+  INSTALL_PATH=/usr/local
+fi
+
 TMP_PATH=$CWD/tmp
 
 if [ -d $TMP_PATH ]; then
   rm -rf $TMP_PATH || exit 1
 fi
-  mkdir -p $TMP_PATH || exit 1
+mkdir -p $TMP_PATH || exit 1
+mkdir -p $CWD/src
 
 # Setup env
-export PKG_CONFIG_PATH=$INSTALL_PATH/lib/pkgconfig
+export PKG_CONFIG_PATH=$INSTALL_PATH/lib/pkgconfig:$INSTALL_PATH/libdata/pkgconfig
 export LD_LIBRARY_PATH=$INSTALL_PATH/lib
 export PATH=/usr/local/bin:$INSTALL_PATH/bin:$PATH
 export QTDIR=$INSTALL_PATH
@@ -52,8 +61,12 @@ export BOOST_ROOT=$INSTALL_PATH
 export OPENJPEG_HOME=$INSTALL_PATH
 export THIRD_PARTY_TOOLS_HOME=$INSTALL_PATH
 
-mkdir -p $INSTALL_PATH/Plugins || exit 1
+if [ "$OS" == "FreeBSD" ]; then
+  export CC=clang
+  export CXX=clang++
+fi
 
+mkdir -p $INSTALL_PATH/Plugins || exit 1
 cd $TMP_PATH || exit 1
 
 git clone $GIT_MISC || exit 1
@@ -72,8 +85,16 @@ git submodule update -i --recursive || exit 1
   tar cvvzf $CWD/src/openfx-misc-$MISC_GIT_VERSION.tar.gz openfx-misc-$MISC_GIT_VERSION
 )
 
-CFLAGS="$BF" CXXFLAGS="$BF" CPPFLAGS="-I${INSTALL_PATH}/include" LDFLAGS="-L${INSTALL_PATH}/lib" make DEBUGFLAG=-O3 BITS=$BIT || exit 1
-cp -a Misc/Linux-$BIT-release/Misc.ofx.bundle $INSTALL_PATH/Plugins/ || exit 1
+if [ "$OS" == "FreeBSD" ]; then
+  # gmake dont honor flags, avoid waisting time just patch ...
+  patch -p0< $CWD/patches/freebsd-openfx-misc-Makefile.diff || exit 1
+  gmake DEBUGFLAG=-O3 BITS=$BIT || exit 1
+  cp -a Misc/FreeBSD-$BIT-release/Misc.ofx.bundle $INSTALL_PATH/Plugins/ || exit 1
+else
+  CFLAGS="$BF" CXXFLAGS="$BF" CPPFLAGS="-I${INSTALL_PATH}/include" LDFLAGS="-L${INSTALL_PATH}/lib" make DEBUGFLAG=-O3 BITS=$BIT || exit 1
+  cp -a Misc/Linux-$BIT-release/Misc.ofx.bundle $INSTALL_PATH/Plugins/ || exit 1
+fi
+
 mkdir -p $INSTALL_PATH/docs/openfx-misc || exit 1
 cp LICENSE README* $INSTALL_PATH/docs/openfx-misc/ || exit 1
 echo $MISC_GIT_VERSION > $INSTALL_PATH/docs/openfx-misc/VERSION || exit 1
@@ -96,8 +117,14 @@ git submodule update -i --recursive || exit 1
   tar cvvzf $CWD/src/openfx-io-$IO_GIT_VERSION.tar.gz openfx-io-$IO_GIT_VERSION
 )
 
-CFLAGS="$BF" CXXFLAGS="$BF" CPPFLAGS="-I${INSTALL_PATH}/include" LDFLAGS="-L${INSTALL_PATH}/lib" make DEBUGFLAG=-O3 BITS=$BIT || exit 1
-cp -a IO/Linux-$BIT-release/IO.ofx.bundle $INSTALL_PATH/Plugins/ || exit 1
+if [ "$OS" == "FreeBSD" ]; then
+  gmake DEBUGFLAG=-O3 BITS=$BIT || exit 1
+  cp -a IO/FreeBSD-$BIT-release/IO.ofx.bundle $INSTALL_PATH/Plugins/ || exit 1
+else
+  CFLAGS="$BF" CXXFLAGS="$BF" CPPFLAGS="-I${INSTALL_PATH}/include" LDFLAGS="-L${INSTALL_PATH}/lib" make DEBUGFLAG=-O3 BITS=$BIT || exit 1
+  cp -a IO/Linux-$BIT-release/IO.ofx.bundle $INSTALL_PATH/Plugins/ || exit 1
+fi
+
 mkdir -p $INSTALL_PATH/docs/openfx-io || exit 1
 cp LICENSE README* $INSTALL_PATH/docs/openfx-io/ || exit 1
 echo $IO_GIT_VERSION > $INSTALL_PATH/docs/openfx-io/VERSION || exit 1
