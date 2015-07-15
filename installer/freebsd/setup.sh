4c400728ffd3de522294694414da044f313b69f2
Ole-André Rodlie <olear@dracolinux.org>
2015-07-15 21:34:35 +0200

    removed freebsd and outdated build script

--- installer/freebsd/setup.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-pkg install glew gmake openimageio opencolorio expat qt4 boost-all ffmpeg pixman xcb-util xcb-util-renderutil pkgconf
--- installer/scripts/Natron-BSD.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-export LC_NUMERIC=C
-DIR="$(cd "$(dirname "$0")" && pwd)"
-export LD_LIBRARY_PATH=$DIR/lib:$LD_LIBRARY_PATH
-$DIR/bin/Natron "$*"
